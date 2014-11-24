de16f88d9a0ba944b73346e7c9dc99b43489300e
godaeun <gowooda@naver.com>
2014-11-24 17:44:54 +0900

    ad

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var y = 0;
 
 function setup() {
-  createCanvas(1166, 1621);
+  createCanvas(1666, 1621);
   img = loadImage("1.png");
   img2 = loadImage("2.png");
   img3 = loadImage("3.png");
@@ -14,7 +14,7 @@ function setup() {
 }
 
 function draw() {
-   background(img7);
+   background(img7, 1166,1621);
   
    fill(255,64,64);
    noStroke()
