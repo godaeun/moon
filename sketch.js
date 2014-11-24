f196fc3570efd16d14a887de3e194938c4c1efc8
godaeun <gowooda@naver.com>
2014-11-24 17:05:18 +0900

    f

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function draw() {
    fill(229,211,136);
    noStroke()
    image(img2,10 + Math.cos(timing*2*PI)*200,           // x좌표
-       750 + Math.sin(timing*2*PI)*200,       // y좌표
+       500 + Math.sin(timing*2*PI)*200,       // y좌표
        50,                                     // width
        50);                                    // height
 
