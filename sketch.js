af1905e002d694950923c8aa239ca227bb59505c
godaeun <gowooda@naver.com>
2014-11-24 17:31:54 +0900

    fd

--- sketch.js
+++ sketch.js
@@ -42,7 +42,7 @@ function draw() {
         70,                                     // width
         70);                                    // height
    //4p
-   var duration = 2500;
+   var duration = 5000;
    var timing = (new Date()%duration)/duration;
 
    fill(200,200,200);
